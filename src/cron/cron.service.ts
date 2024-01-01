09bfd4a246938a863242c3e8369ee1bc69eff0e8
Saumyapratim Das <saumyadas2017@gmail.com>
2024-01-01 11:13:09 +0530

    cron setup

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -5,9 +5,9 @@ import * as cron from 'node-cron';
 @Injectable()
 export class CronService {
   startCronJob(): void {
-    cron.schedule('*/10 * * * *', async () => {
+    cron.schedule('0/10 * * * *', async () => {
         try {
-          const response = await axios.get('http://localhost:3002/admin/api-key');
+          const response = await axios.get('https://telebot2-xvgp.onrender.com/admin/api-key');
           console.log('Ping successful:', response.status);
         } catch (error) {
           console.error('Ping failed:', error.message);
